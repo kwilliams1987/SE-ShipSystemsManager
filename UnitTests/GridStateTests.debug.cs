c3c1e1452d5f85f4be31e1d47007e8dfccd1ecc3
Kristian Williams <kristian@softtech.nl>
2019-04-18 16:07:36 +0200

    - Cleaned up old bootstrapper. - Started experimenting with adding MDK Framework to a new unit test project.

---
+++ UnitTests/GridStateTests.debug.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using IngameScript;
+using Malware.MDKUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sandbox.ModAPI.Ingame;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class GridStateTests
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            MDKUtilityFramework.Load();
+        }
+
+        [TestMethod]
+        public void TestDefault()
+        {
+            var program = MDKFactory.CreateProgram<Program>();
+            MDKFactory.Run(program, updateType: UpdateType.Once);
+        }
+    }
+}
