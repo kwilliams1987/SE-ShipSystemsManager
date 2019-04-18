3d999e2dc42421ad434337510db429755e6bb5c1
Kristian Williams <kristian@softtech.nl>
2019-04-18 17:23:40 +0200

    - Reimplemented `group add zone` and `group add function` commands.

--- ShipSystemsManager/Program.cs
+++ ShipSystemsManager/Program.cs
@@ -3,6 +3,7 @@ using Sandbox.ModAPI.Ingame;
 using System.Linq;
 using System;
 using System.Collections.Generic;
+using VRage.Game.ModAPI.Ingame.Utilities;
 
 namespace IngameScript
 {
@@ -158,6 +159,107 @@ namespace IngameScript
                             else
                             {
                                 Echo($"Self Destruct timer must be a decimal value greater than 0.");
+                            }
+                            break;
+                    }
+                    break;
+                case "group":
+                    var verb = words.ElementAtOrDefault(1) + " " + words.ElementAtOrDefault(2);
+                    var parameters = String.Join(" ", words.Skip(3)).Split(';');
+
+                    if (parameters.Count() == 0 || parameters.Count() > 2)
+                        break;
+
+                    var group = parameters.ElementAt(0);
+                    var target = parameters.ElementAtOrDefault(1);
+
+                    var blocks = new List<IMyTerminalBlock>();
+                    var config = new MyIni();
+                    var values = new List<String>();
+
+                    GridTerminalSystem.GetBlockGroupWithName(group).GetBlocks(blocks);
+
+                    if (!blocks.Any())
+                        break;
+
+                    switch (verb)
+                    {
+                        case "add function":
+                        case "function add":
+                            if (target == "")
+                                break;
+
+                            foreach (var block in blocks)
+                            {
+                                config.TryParse(block.CustomData);
+                                config.Get(ConfigSection, "functions").GetLines(values);
+
+                                if (!values.Contains(target))
+                                {
+                                    values.Add(target);
+                                    config.Set(ConfigSection, "functions", String.Join("\n", values));
+
+                                    block.CustomData = config.ToString();
+                                }
+                            }
+                            break;
+                        case "remove function":
+                        case "function remove":
+                            foreach (var block in blocks)
+                            {
+                                config.TryParse(block.CustomData);
+                                config.Get(ConfigSection, "functions").GetLines(values);
+
+                                if (target == "")
+                                {
+                                    config.Delete(ConfigSection, "functions");
+                                    block.CustomData = config.ToString();
+                                }
+                                else if (values.Contains(target))
+                                {
+                                    values.Remove(target);
+                                    config.Set(ConfigSection, "functions", String.Join("\n", values));
+
+                                }
+                            }
+                            break;
+                        case "add zone":
+                        case "zone add":
+                            if (target == "")
+                                break;
+
+                            foreach (var block in blocks)
+                            {
+                                config.TryParse(block.CustomData);
+                                config.Get(ConfigSection, "zones").GetLines(values);
+
+                                if (!values.Contains(target))
+                                {
+                                    values.Add(target);
+                                    config.Set(ConfigSection, "zones", String.Join("\n", values));
+
+                                    block.CustomData = config.ToString();
+                                }
+                            }
+                            break;
+                        case "remove zone":
+                        case "zone remove":
+                            foreach (var block in blocks)
+                            {
+                                config.TryParse(block.CustomData);
+                                config.Get(ConfigSection, "zones").GetLines(values);
+
+                                if (target == "")
+                                {
+                                    config.Delete(ConfigSection, "zones");
+                                    block.CustomData = config.ToString();
+                                }
+                                else if (values.Contains(target))
+                                {
+                                    values.Remove(target);
+                                    config.Set(ConfigSection, "zones", String.Join("\n", values));
+
+                                }
                             }
                             break;
                     }
